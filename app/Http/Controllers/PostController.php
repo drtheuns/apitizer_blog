b3f2bf5d482904487d57e1ed5a6dc3d52b7358a1
Randall Theuns <randall@theuns.org>
2020-01-11 00:26:52 +0100

    Fix config

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -10,7 +10,6 @@ class PostController
 {
     public function index(Request $request)
     {
-        // return PostBuilder::make($request)->paginate();
         return PostBuilder::make($request)->paginate();
     }
 
